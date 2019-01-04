a26453e5a95be27237cb2b71f217ce032e2fcfb3
levosos <razhaleva@gmail.com>
2019-01-04 21:30:54 +0200

    supporting playing the changed part only
    added second counter

--- src/components/Wavesurfer.js
+++ src/components/Wavesurfer.js
@@ -10,6 +10,21 @@ import Forward5Icon from '@material-ui/icons/Forward5';
 import Replay5Icon from '@material-ui/icons/Replay5';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return minutes+':'+seconds;
+  // return hours+':'+minutes+':'+seconds;
+}
 
 const styles = _ => ({
   bigIcon: {
@@ -31,6 +46,7 @@ class Wavesurfer extends React.Component {
     pos: 0,
     duration: 0,
     loaded: false,
+    audio: 'audio/full.mp3',
   };
 
   handleTogglePlay = _ => {
@@ -55,7 +71,7 @@ class Wavesurfer extends React.Component {
   
   handleNext = _ => {
     var newPos = this.state.pos + 5;
-    if (newPos > this.state.duration) { newPos = this.state.duration }
+    if (newPos >= this.state.duration) { newPos = this.state.duration - 1 }
     this.setState({
       pos: newPos
     });
@@ -65,6 +81,14 @@ class Wavesurfer extends React.Component {
     this.setState({
       duration: e.wavesurfer.getDuration(),
       loaded: true,
+    });
+  }
+  
+  changeAudio = (event, _) => {
+    this.setState({
+      audio: event.target.value,
+      playing: false,
+      pos: 0,
     });
   }
 
@@ -85,8 +109,19 @@ class Wavesurfer extends React.Component {
           {!this.state.loaded && <div className={classes.progress}>
             <CircularProgress />
           </div>}
+          {this.state.loaded && 
+            <>
+              <span>I want to listen to the </span>
+              <Select
+                  value={this.state.audio}
+                  onChange={this.changeAudio}
+                >
+                  <MenuItem value='audio/part.mp3'>changed part</MenuItem>
+                  <MenuItem value='audio/full.mp3'>entire song</MenuItem>
+              </Select>
+            </>}
           <Ws
-            audioFile={'audio/ohdarling.mpeg'}
+            audioFile={this.state.audio}
             pos={this.state.pos}
             onPosChange={this.handlePosChange}
             playing={this.state.playing}
@@ -107,22 +142,28 @@ class Wavesurfer extends React.Component {
             justify="center"
             alignItems="center"
           >
-            <Tooltip title="Replay 5 seconds">
-              <IconButton onClick={this.handlePrevious} disabled={!this.state.loaded}>
-                <Replay5Icon className={classes.smallIcon}/>
-              </IconButton>
-            </Tooltip>
-            <Tooltip title={this.state.playing ? "Pause" : "Play"}>
-              <IconButton onClick={this.handleTogglePlay} disabled={!this.state.loaded}>
-                {!this.state.playing && <PlayArrowIcon className={classes.bigIcon}/>}
-                {this.state.playing && <PauseIcon className={classes.bigIcon}/>}
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Skip 5 seconds">
-              <IconButton onClick={this.handleNext} disabled={!this.state.loaded}>
-                <Forward5Icon className={classes.smallIcon}/>
-              </IconButton>
-            </Tooltip>
+            <Grid item xs>
+              {this.state.pos.toString().toHHMMSS()} / {this.state.duration.toString().toHHMMSS()}
+            </Grid>
+            <Grid item xs={6}>
+              <Tooltip title="Replay 5 seconds">
+                <IconButton onClick={this.handlePrevious} disabled={!this.state.loaded}>
+                  <Replay5Icon className={classes.smallIcon}/>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title={this.state.playing ? "Pause" : "Play"}>
+                <IconButton onClick={this.handleTogglePlay} disabled={!this.state.loaded}>
+                  {!this.state.playing && <PlayArrowIcon className={classes.bigIcon}/>}
+                  {this.state.playing && <PauseIcon className={classes.bigIcon}/>}
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Skip 5 seconds">
+                <IconButton onClick={this.handleNext} disabled={!this.state.loaded}>
+                  <Forward5Icon className={classes.smallIcon}/>
+                </IconButton>
+              </Tooltip>
+            </Grid>
+            <Grid item xs/>
           </Grid>
         </Grid>
       </Grid>
